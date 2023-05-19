73d9fcc24bcd036bf18d30bc816a8d11898550eb
George Amuzu <gnamuzu@gmail.com>
2023-05-19 08:39:35 +0100

    created Appointment model.

---
+++ src/main/java/com/george/doctors_appointment_portal/model/Appointment.java
@@ -0,0 +1,67 @@
+package com.george.doctors_appointment_portal.model;
+
+public class Appointment {
+    private String appointmentID;
+    private String userID;
+    private String specialityID;
+    private String doctorID;
+    private String appointmentDate;
+    private String description;
+    private String status;
+
+    public String getAppointmentID() {
+        return appointmentID;
+    }
+
+    public void setAppointmentID(String appointmentID) {
+        this.appointmentID = appointmentID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getSpecialityID() {
+        return specialityID;
+    }
+
+    public void setSpecialityID(String specialityID) {
+        this.specialityID = specialityID;
+    }
+
+    public String getDoctorID() {
+        return doctorID;
+    }
+
+    public void setDoctorID(String doctorID) {
+        this.doctorID = doctorID;
+    }
+
+    public String getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public void setAppointmentDate(String appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
