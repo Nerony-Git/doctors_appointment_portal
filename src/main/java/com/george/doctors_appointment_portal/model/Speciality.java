c8614dd1c4f5c6733cef0f66c6c6c5e44bb58d45
George Amuzu <gnamuzu@gmail.com>
2023-05-19 08:39:19 +0100

    created Speciality model

---
+++ src/main/java/com/george/doctors_appointment_portal/model/Speciality.java
@@ -0,0 +1,22 @@
+package com.george.doctors_appointment_portal.model;
+
+public class Speciality {
+    private String sID;
+    private String specialityName;
+
+    public String getsID() {
+        return sID;
+    }
+
+    public void setsID(String sID) {
+        this.sID = sID;
+    }
+
+    public String getSpecialityName() {
+        return specialityName;
+    }
+
+    public void setSpecialityName(String specialityName) {
+        this.specialityName = specialityName;
+    }
+}
