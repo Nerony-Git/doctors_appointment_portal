5504dce11b6a54e23729fb87662554f970edcdc0
George Amuzu <gnamuzu@gmail.com>
2023-05-19 08:39:49 +0100

    created Doctor model.

---
+++ src/main/java/com/george/doctors_appointment_portal/model/Doctor.java
@@ -0,0 +1,106 @@
+package com.george.doctors_appointment_portal.model;
+
+import java.io.Serializable;
+
+public class Doctor implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String userID;
+    private String firstName;
+    private String lastName;
+    private String otherName;
+    private String email;
+    private String username;
+    private String password;
+    private String contact;
+    private String dob;
+    private String speciality;
+    private String qualification;
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getOtherName() {
+        return otherName;
+    }
+
+    public void setOtherName(String otherName) {
+        this.otherName = otherName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    public String getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+}
