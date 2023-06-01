5ef23f1b446eadbe990e081b4774c4336b373c2c
George Amuzu <gnamuzu@gmail.com>
2023-06-01 15:29:25 +0100

    added admin login, logout and register webservlet path.

--- src/main/java/com/george/doctors_appointment_portal/controller/AdminController.java
+++ src/main/java/com/george/doctors_appointment_portal/controller/AdminController.java
@@ -1,4 +1,71 @@
 package com.george.doctors_appointment_portal.controller;
 
-public class AdminController {
+import com.george.doctors_appointment_portal.dao.AdminDao;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet({"/admin_login", "/admin_logout", "/admin_register"})
+public class AdminController extends HttpServlet {
+    private AdminDao adminDao;
+
+    public void init(){
+        adminDao = new AdminDao();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getServletPath();
+
+        try {
+            switch (action) {
+                case "/admin_login":
+                    adminLogin(request, response);
+                    break;
+                case "/admin_register":
+                    adminRegister(request, response);
+                    break;
+                case "/admin_logout":
+                    adminLogout(request, response);
+                    break;
+                default:
+                    RequestDispatcher dispatcher = request.getRequestDispatcher("pages/admin/admin_login.jsp");
+                    dispatcher.forward(request, response);
+
+            }
+        } catch (Exception ex) {
+            throw new ServletException(ex);
+        }
+    }
+
+    private void adminLogout(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        session.removeAttribute("user");
+        session.setAttribute("successMsg", "Successfully Logout");
+        response.sendRedirect("pages/admin/admin_login.jsp");
+    }
+
+    private void adminLogin(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("pages/admin/admin_login.jsp");
+        dispatcher.forward(request, response);
+
+    }
+
+    private void adminRegister(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("pages/admin/admin_register.jsp");
+        dispatcher.forward(request, response);
+
+    }
+
 }
