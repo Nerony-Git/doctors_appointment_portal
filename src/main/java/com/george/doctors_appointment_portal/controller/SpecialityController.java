09af3f18a3a178ef10235b918874d16cf946ebde
George Amuzu <gnamuzu@gmail.com>
2023-06-02 20:40:29 +0100

    updated Speciality controller.

--- src/main/java/com/george/doctors_appointment_portal/controller/SpecialityController.java
+++ src/main/java/com/george/doctors_appointment_portal/controller/SpecialityController.java
@@ -1,4 +1,34 @@
 package com.george.doctors_appointment_portal.controller;
 
+import com.george.doctors_appointment_portal.dao.SpecialityDao;
+import com.george.doctors_appointment_portal.model.Speciality;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet({"/list_speciality", "listSpeciality"})
 public class SpecialityController {
+
+    private SpecialityDao specialityDao;
+
+    public void init() {
+        specialityDao = new SpecialityDao();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getServletPath();
+
+    }
+
+
 }
