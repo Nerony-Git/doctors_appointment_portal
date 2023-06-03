1f99ae4c1ef53f35b14cb73ce16be1f3ed43a31b
George Amuzu <gnamuzu@gmail.com>
2023-06-03 21:33:54 +0100

    added get speciality name method.

--- src/main/java/com/george/doctors_appointment_portal/dao/SpecialityDao.java
+++ src/main/java/com/george/doctors_appointment_portal/dao/SpecialityDao.java
@@ -13,6 +13,8 @@ import java.util.List;
 public class SpecialityDao {
 
     private static final String SELECT_ALL_SPECIALITY = "SELECT * FROM speciality";
+    private static final String SELECT_SPECIALITY_BY_ID = "SELECT * FROM speciality WHERE sid = ?";
+    private static final String GET_SPECIALITY_NAME = "SELECT speciality_name FROM speciality WHERE sid = ?";
     private static final String INSERT_SPECIALITY = "INSERT INTO speciality (sid, speciality_name) VALUES (?, ?)";
 
     public void insertSpeciality(Speciality speciality) throws SQLException {
@@ -46,5 +48,38 @@ public class SpecialityDao {
         }
         return specialities;
 
+    }
+
+    public Speciality getSpecialityByID(String id) throws SQLException {
+        Speciality speciality = null;
+        try (Connection connection = JDBCUtils.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_SPECIALITY_BY_ID)){
+            preparedStatement.setString(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                String sid = resultSet.getString("sid");
+                String specialityName = resultSet.getString("speciality_name");
+
+                speciality = new Speciality(sid, specialityName);
+            }
+        }
+        return speciality;
+    }
+
+    public String getSpecialityName(String id) throws SQLException {
+        String specialityName = null;
+        try (Connection connection = JDBCUtils.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(GET_SPECIALITY_NAME)){
+            preparedStatement.setString(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()){
+                specialityName = resultSet.getString("speciality_name");
+
+                System.out.println(specialityName);
+            }
+        }
+        return specialityName;
     }
 }
