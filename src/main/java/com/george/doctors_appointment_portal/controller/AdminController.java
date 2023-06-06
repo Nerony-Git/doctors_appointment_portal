065db8a048a985db897cead2c082944e165e8bd9
George Amuzu <gnamuzu@gmail.com>
2023-06-06 09:44:48 +0100

    added get all specialties method, path and servlet.

--- src/main/java/com/george/doctors_appointment_portal/controller/AdminController.java
+++ src/main/java/com/george/doctors_appointment_portal/controller/AdminController.java
@@ -2,9 +2,11 @@ package com.george.doctors_appointment_portal.controller;
 
 import com.george.doctors_appointment_portal.dao.AdminDao;
 import com.george.doctors_appointment_portal.dao.DoctorDao;
+import com.george.doctors_appointment_portal.dao.SpecialityDao;
 import com.george.doctors_appointment_portal.dao.UserDao;
 import com.george.doctors_appointment_portal.model.Admin;
 import com.george.doctors_appointment_portal.model.Doctor;
+import com.george.doctors_appointment_portal.model.Speciality;
 import com.george.doctors_appointment_portal.model.User;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -23,12 +25,13 @@ import java.util.List;
 @WebServlet({
         "/admin_login", "/admin_logout", "/admin_register", "/admin_authenticate", "/admin_dashboard",
         "/new_admin", "/admin_view", "/admin_edit", "/admin_password", "/admin_change", "/admin_update",
-        "/doctors", "/users"
+        "/doctors", "/users", "/specialties"
 })
 public class AdminController extends HttpServlet {
     private AdminDao adminDao = new AdminDao();
     private DoctorDao doctorDao = new DoctorDao();
     private UserDao userDao = new UserDao();
+    private SpecialityDao specialityDao = new SpecialityDao();
     private static final DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     public void init(){
@@ -84,6 +87,9 @@ public class AdminController extends HttpServlet {
                     break;
                 case "/users":
                     getUsers(request, response);
+                    break;
+                case "/specialties":
+                    getSpecialties(request, response);
                     break;
                 default:
                     RequestDispatcher dispatcher = request.getRequestDispatcher("pages/admin/admin_login.jsp");
@@ -265,6 +271,13 @@ public class AdminController extends HttpServlet {
         List<User> userList = userDao.getAllUsers();
         request.setAttribute("userList", userList);
         RequestDispatcher dispatcher = request.getRequestDispatcher("pages/admin/users.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void getSpecialties(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException, ServletException {
+        List<Speciality> specialityList = specialityDao.getAllSpeciality();
+        request.setAttribute("specialityList", specialityList);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("pages/admin/specialties.jsp");
         dispatcher.forward(request, response);
     }
 
